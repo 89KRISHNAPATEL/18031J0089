336cff605e1b714b4f9c60595e6cb1a6a554f245
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2018-10-04 22:24:08 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-1/Module_3/Solution3sum.java
@@ -0,0 +1,24 @@
+package Module2;
+
+import java.util.Scanner;
+
+public class Solution3sum {   
+	 public static int count(int[] a)    { 
+		 // Count triples that sum to 0.      
+		 int N = a.length;     
+		 int cnt = 0;    
+		 for (int i = 0; i < N; i++)        
+			 for (int j = i+1; j < N; j++)            
+				 for (int k = j+1; k < N; k++)               
+					 if (a[i] + a[j] + a[k] == 0)                  
+						 cnt++;     
+		 return cnt;   
+		 }  
+	 public static void main(String[] args)    { 
+		 Scanner scn=new Scanner(System.in);
+		 int n=scn.nextInt();
+		 int[] a = new int[n];   
+		 System.out.println(count(a));   
+	 }
+}
+
