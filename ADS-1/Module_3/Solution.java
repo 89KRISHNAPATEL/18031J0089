ac6cebeb5bc42b94ae65e3474c8e38285f370387
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2018-10-04 23:16:52 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1/Module_3/Solution.java
+++ ADS-1/Module_3/Solution.java
@@ -2,24 +2,27 @@ package Module2;
 
 import java.util.Scanner;
 
-public class Solution {   
-    public static int count(int[] a){ 
-          // Count triples that sum to 0.      
-          int N = a.length;     
-          int cnt = 0;    
-          for (int i = 0; i < N; i++)        
-               for (int j = i+1; j < N; j++)            
-	            for (int k = j+1; k < N; k++)               
-		         if (a[i] + a[j] + a[k] == 0)                  
-			     cnt++; 
-		   
-      return cnt;   
-  }  
-public static void main(String[] args)    { 
-	Scanner scn=new Scanner(System.in);
-	int n=scn.nextInt();
-	int[] a = new int[n];   
-	System.out.println(count(a));   
- }
+public class Solution{   
+	 public  int count(int[] a){ 
+		 // Count triples that sum to 0.      
+		 int N = a.length;     
+		 int cnt = 0;    
+		 for (int i = 0; i < N; i++)        
+			 for (int j = i+1; j < N; j++)            
+				 for (int k = j+1; k < N; k++)               
+					 if (a[i] + a[j] + a[k] == 0)                  
+						 cnt++;     
+		 return cnt;   
+		 }  
+	 public static void main(String[] args)    { 
+		 Scanner scn=new Scanner(System.in);
+		 int n=scn.nextInt();
+		 int[] a = new int[n]; 
+		 Solution ss=new Solution();
+		 for(int i=0;i<n;i++) {
+			 a[i]=scn.nextInt();
+		 }
+		 System.out.println(ss.count(a));   
+	 }
 }
 
